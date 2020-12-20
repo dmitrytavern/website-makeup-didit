eb763a9a06a5b3e4d14737e624cd66f42160f190
dogger-work <dogger.work@gmail.com>
2020-12-20 16:31:41 +0200

    0.1.1v - Added menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,31 @@
 import '../sass/app.sass'
+
+//  Header
+
+const $header         = $('.header')
+const $menu           = $('.menu')
+const $headerMenuBtn  = $('#header-menu-btn')
+
+
+function scrollPage() {
+    if (window.pageYOffset > 0) {
+        $header.addClass('is-scroll')
+    } else {
+        $header.removeClass('is-scroll')
+    }
+
+    // setTimeout(function () {
+        if (window.innerWidth < 992) $menu.css('padding-top', $header.outerHeight(true))
+    // }, 300)
+}
+
+
+function toggleHeaderMenu() {
+    $headerMenuBtn.toggleClass('is-active')
+    $header.toggleClass('is-menu-open')
+    $menu.toggleClass('is-open')
+    document.body.classList.toggle('menu-opened')
+}
+
+$($headerMenuBtn).on('click', toggleHeaderMenu)
+$(window).on('resize scroll load', scrollPage)
